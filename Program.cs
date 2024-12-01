c49a1e6edc23e1b9cf3348c6014cc1e132aaba60
Maksym Solomyanov <maksym.solomyanov@gmail.com>
2024-12-01 16:05:43 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿
+
+using System.Collections;
+
+var customCollection = new CustomCollection(
+    new string[] { "aaa", "bbb", "ccc" });
+
+foreach (var word in customCollection)
+{
+    Console.WriteLine(word);
+}
+
+Console.ReadKey();
+
+public class CustomCollection : IEnumerable<string>
+{
+    public string[] Words { get; }
+
+    public CustomCollection(string[] words)
+    {
+        Words = words;
+    }
+
+    public string this[int index]
+    {
+        get => Words[index];
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public IEnumerator<string> GetEnumerator()
+    {
+        return new WordsEnumerator(Words);
+    }
+}
+
+public class WordsEnumerator : IEnumerator<string>
+{
+    private const int InitialPosition = -1;
+    private int _currentPosition = InitialPosition;
+    private readonly string[] _words;
+
+    public WordsEnumerator(string[] words)
+    {
+        _words = words;
+    }
+
+    object IEnumerator.Current => Current;
+
+    public string Current
+    {
+        get
+        {
+            try
+            {
+                return _words[_currentPosition];
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                throw new IndexOutOfRangeException(
+                    $"{nameof(CustomCollection)}'s end reached.",
+                    ex);
+            }
+        }
+    }
+
+    public bool MoveNext()
+    {
+        ++_currentPosition;
+        return _currentPosition < _words.Length;
+    }
+
+    public void Reset()
+    {
+        _currentPosition = InitialPosition;
+    }
+
+    public void Dispose()
+    {
+
+    }
+}
